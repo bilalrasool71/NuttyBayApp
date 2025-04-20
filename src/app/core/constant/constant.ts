e13a27ae11b39f0a7cc18a6e0bacb4297ee7ffbb
Zakir-Islam <islamzakir964@gmail.com>
2025-04-20 10:53:43 +0500

    added firebase

--- src/app/core/constant/constant.ts
+++ src/app/core/constant/constant.ts
@@ -1,6 +1,6 @@
-// export const baseURL="https://angelfood-api.webappconsulting.com.au/";
-export const baseURL="https://localhost:7148/";
+export const baseURL="https://nuttybayapi.webappconsulting.com.au/";
+//export const baseURL="https://localhost:7148/";
 export const apiBaseURL=`${baseURL}api/`
 
 //export const webURL=`http://localhost:4200/`
-export const webURL=`https://angel-food-nz.web.app/`
+export const webURL=`https://nuttybay-app.web.app/`
