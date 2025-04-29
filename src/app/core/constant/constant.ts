df636abca477442df4f860532ac553ea893f87e6
Zakir-Islam <islamzakir964@gmail.com>
2025-04-29 15:55:34 +0500

    added bucket url

--- src/app/core/constant/constant.ts
+++ src/app/core/constant/constant.ts
@@ -1,10 +1,10 @@
- export const baseURL="https://nuttybayapi.webappconsulting.com.au/";
-//export const baseURL="https://localhost:7148/";
+ //export const baseURL="https://nuttybayapi.webappconsulting.com.au/";
+export const baseURL="https://localhost:7148/";
 export const apiBaseURL=`${baseURL}api/`
 
-//export const webURL=`http://localhost:4200/`
-export const webURL=`https://nuttybay-app.web.app/`
+export const webURL=`http://localhost:4200/`
+//export const webURL=`https://nuttybay-app.web.app/`
 
 
 export const pdfKey="aeeacfb1-bcf3-4c55-bb99-10e112829bfa";
-export const s3ProductionPdfpath = "https://angelfood-bucket.s3.ap-southeast-2.amazonaws.com/"
+export const s3ProductionPdfpath = "https://nuttybaybucket.s3.ap-southeast-2.amazonaws.com/"
